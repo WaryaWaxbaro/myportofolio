f450df26010990d14d9507f906c0de91ddb9d680
Warya Waxbaro <warya.waxbaro@gmail.com>
2019-08-27 16:59:30 +0300

    Updates

---
+++ precache-manifest.c6a453ae3fc35d746f4129b12409ea39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06110c250c88f41a638f117bf9a4d999",
+    "url": "/myportofolio/index.html"
+  },
+  {
+    "revision": "cd75b5bd459fea520ee0",
+    "url": "/myportofolio/static/css/main.6ba88be0.chunk.css"
+  },
+  {
+    "revision": "b2fa55dfdd4d28dd1a6e",
+    "url": "/myportofolio/static/js/2.721e4fbc.chunk.js"
+  },
+  {
+    "revision": "cd75b5bd459fea520ee0",
+    "url": "/myportofolio/static/js/main.61464b8b.chunk.js"
+  },
+  {
+    "revision": "c191d80d5eb75583b05a",
+    "url": "/myportofolio/static/js/runtime~main.be2b7e6e.js"
+  },
+  {
+    "revision": "4a669a4bad737b4943afcabf23f4453e",
+    "url": "/myportofolio/static/media/profile.4a669a4b.png"
+  }
+]);
