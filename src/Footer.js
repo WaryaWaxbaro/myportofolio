a8dfda0ff3999e575d4639fb42d37986adeed799
WaryaWaxbaro <warya.waxbaro@gmail.com>
2019-09-02 12:21:30 +0300

    add footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import './scss/_footer.scss';
 class Footer extends Component {
     render() { 
         return ( <div className="Footer">
-            &copy; {`${new Date().getFullYear()}`} shakurmoller.com
+            &copy; {`${new Date().getFullYear()} `} <a className="FooterLink" href="http://shakurmoller.com" target="_blank" rel="noopener noreferrer"> shakurmoller.com</a>All rights reserved.
         </div> );
     }
 }
